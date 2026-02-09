e97f60b2819612ba479b3574d3b4477624d7d642
Jo Wagner <jowagner@limago.de>
2026-02-09 14:49:21 +0100

    initial commit

--- WebApp/src/main/java/de/fi/webapp/persistence/entity/PersonEntity.java
+++ WebApp/src/main/java/de/fi/webapp/persistence/entity/PersonEntity.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
+
 
 import java.util.Objects;
 import java.util.UUID;
